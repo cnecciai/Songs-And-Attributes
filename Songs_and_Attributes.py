219f08e6f485f43c7d34733dce29c581743ee702
Clark Necciai <44730537+cnecciai@users.noreply.github.com>
2021-10-03 18:26:31 -0400

    Update Songs_and_Attributes.py

--- Songs_and_Attributes.py
+++ Songs_and_Attributes.py
@@ -1,22 +1,31 @@
+#Clark P. Necciai
+
 import spotipy as spotify
+import pandas as pd
+import matplotlib.pyplot as plt
 from openpyxl import Workbook
 from openpyxl.worksheet.table import Table, TableStyleInfo
 from spotipy.oauth2 import SpotifyClientCredentials
 
-cid = 'USER CID'#   <-----YOUR OWN SPOTIFY ID
-secret = 'SECRET'#  <-----YOUR OWN SECRET CODE
+#Identifiers necessary to utilize spotify API
+cid = '7c7aa14d1c08482f9df34d7a732255e2'
+secret = 'f718d8388dd64c7b8bc0f699be40752d'
 
 client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
 
+#Create microsoft excel using openpyxl
 workbook = Workbook()
-workbook.save(filename="keysAndSongs.xlsx")
+workbook.save(filename="KeysAndSongs.xlsx")
 ws = workbook.active
 ws.title = "Songs And Attributes"
 
+#Access Spotify Credentials/API
 sp = spotify.Spotify(client_credentials_manager = client_credentials_manager)
 
+#Add first row of titular characteristics
 ws.append(["Artist/Composer", "Title", "Length", "Key", "Tempo","Time Signature", "Release Date", "Energy", "Loudness", "Valence", "Danceability", "Acousticness"])
 
+#Convert to readable time for table
 def convertMillis(millis):
     seconds=(millis/1000)%60
     seconds = int(seconds)
@@ -26,7 +35,9 @@ def convertMillis(millis):
     minutes = int(minutes)
     time = str(minutes) + ":"+ str(seconds)
     return time
-    
+
+#Spotify API utilizes numerical system instead of keys
+#Built a quick table for conversions during read in from API
 def detKey(keyValue):
     switcher = {
     0: "C",
@@ -45,14 +56,16 @@ def detKey(keyValue):
     }
     return str(switcher.get(keyValue, "Not Known"))
 
+#Number of songs decision by user
 songCount = int(input("How many songs: "))
 
-#REPLACE PLAYLIST_ID VALUE WITH ANY UIC FROM ANY SPOTIFY PLAYLIST
+#Access list features from 0-songcount and return Attributes of songs
+#of any playlist --> playlist ID can be changed to change playlist desired
+# Ability to utilize own playlist possible
 for i in range(0, songCount):
     playlist_items_list = sp.playlist_items(playlist_id = '2LOxEzC4KmoWJ9NhW0kz5M', fields = 'items', limit = 1, offset = i)
 
     features = sp.audio_features(playlist_items_list['items'][0]['track']['id'])
-
     energy = features[0]['energy']
     danceability = features[0]['danceability']
     valence = features[0]['valence']
@@ -70,6 +83,7 @@ for i in range(0, songCount):
     time = PlayListItemsFull['duration_ms']
     time = convertMillis(time)
 
+    #Format cells with appropriate information on every iteration
     ws.cell(row = i + 2,column = 1,value = bandname)
     ws.cell(row = i + 2,column = 2,value = song_title)
     ws.cell(row = i + 2,column = 3,value = time)
@@ -84,4 +98,5 @@ for i in range(0, songCount):
     ws.cell(row = i + 2,column = 12,value = acousticness)
     #End range
 
+#Save and return notebook in same file
 workbook.save(filename="KeysAndSongs.xlsx")
